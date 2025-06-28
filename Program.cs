1ecce140aeb1d29ffb2094e4934ad2f700fc689e
marwan_farook <marwanfarook99@gmailcom>
2025-06-28 23:44:58 +0300

    Revert "Change From White To Cyan Program.cs"
    This reverts commit ab33abeb2054c13a968e493b1793c9bc837204b6.

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ using System.Linq;
 var Todo = new List<string>();
 Console.ForegroundColor = ConsoleColor.DarkBlue;
 Console.WriteLine(" Hello There In Our Todo Project !");
-Console.ForegroundColor = ConsoleColor.Cyan;
+Console.ForegroundColor = ConsoleColor.White;
 bool ExitLoop = true;
 while (ExitLoop)
 {
